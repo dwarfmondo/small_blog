50a8015d0b6a1d7c673e57907b76369f9ebf4d27
Matt Vasquez <mattvasquez@gmail.com>
2016-08-26 15:21:53 -0500

    Includes tests and ability to create a blog

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,51 @@
+class BlogsController < ApplicationController
+  before_action :require_user, except: [:index, :show]
+
+  def index
+    @blogs = Blog.includes(:user).order('created_at DESC')
+  end
+
+  def show
+    load_blog
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = current_user.blogs.build(blog_params)
+
+    if @blog.save
+      redirect_to @blog
+    else
+      render :new
+    end
+  end
+
+  def edit
+    load_blog
+  end
+
+  def update
+    load_blog
+  end
+
+  def delete
+    load_blog
+  end
+
+  def destroy
+    load_blog
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:name, :slug)
+  end
+
+  def load_blog
+    @blog ||= Blog.includes(:user).find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+
+  get 'blogs/show'
+
   get     '/signup',  to: 'users#new'
   get     '/signin',  to: 'sessions#new'
   post    '/signin',  to: 'sessions#create'
   delete  '/signout', to: 'sessions#destroy'
 
   resources :users, except: :index do
+    member do
+      get :delete
+    end
+  end
+
+  resources :blogs do
     member do
       get :delete
     end
