0eb0f16bf67fe63e631016e68d28433879ae043e
Matt Vasquez <mattvasquez@gmail.com>
2016-08-29 01:23:48 -0500

    Fixes a broken test

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe BlogsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      blog = create(:blog)
+      get :show, id: blog
       expect(response).to have_http_status(:success)
     end
   end
