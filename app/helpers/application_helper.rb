4bc1c865efa149a102da1ba7b1390e5f7b377411
Matt Vasquez <mattvasquez@gmail.com>
2016-08-28 22:56:21 -0500

    Displays a blog post and renders markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,31 @@ module ApplicationHelper
       render 'error_messages', model: model
     end
   end
+
+  def markdown(text)
+    @render_extensions ||= {
+      hard_wrap: true,
+      filter_html: true,
+      safe_links_only: true,
+      fenced_code_blocks: true,
+      link_attributes: {
+        rel: 'nofollow',
+        target: "_blank"
+      },
+      lax_spacing: true,
+      autolink: true,
+      strikethrough: true,
+      tables: true,
+      highlight: true,
+      underline: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true
+    }
+
+    @md ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, @render_extensions)
+    html = @md.render(text).html_safe
+
+    content_tag(:div, html, class: "markdown")
+
+  end
 end
